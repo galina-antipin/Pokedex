b4a2ef7f94ca78dbdd18325ce4a619b32b1e8045
Galina Antipin <galina.antipin@gmail.com>
2024-07-25 14:16:16 +0200

    added pokemonDetails function

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 let allPokemons = [];
 let currentStart = 1;
 let currentEnd = 20;
+let currentIndex = 0;
+let currentPokemonIndex = 0;
 
 async function fetchData(start, end) {
     for (let i = start; i <= end; i++) {
@@ -30,7 +32,7 @@ function addPokemonHtml(pokemon, imageUrl, typesHtml, typeClass, i) {
         <div class="pokemon-card ${typeClass}" onclick="openPokemonDetails(${i})">
             <h2>${pokemon.name}</h2>
             <img src="${imageUrl}">
-         <div class="types-container">${typesHtml}</div>
+            <div class="types-container">${typesHtml}</div>
         </div>
     `;
 }
@@ -41,13 +43,51 @@ function loadMore() {
     fetchData(currentStart, currentEnd);
 }
 
-fetchData(currentStart, currentEnd);
-
 function filterAndShowNames(filterWord) {
     let value = document.getElementById('searchBox').value;
-    if (value.length < 3){
+    if (value.length < 3) {
         return;
     }
     let filteredPokemons = allPokemons.filter(pokemon => pokemon.name.toLowerCase().includes(filterWord.toLowerCase()));
     renderPokemonData(filteredPokemons);
+}
+
+fetchData(currentStart, currentEnd);
+
+function openPokemonDetails(i, pokemons = allPokemons) {
+    currentPokemonIndex = i; // Setze den aktuellen Pokémon-Index
+    document.getElementById('overlay').classList.remove('d-none');
+    let pokemonDetails = document.getElementById('overlay');
+    let pokemon = pokemons[i];
+    let imageUrl = pokemon.sprites['other']['official-artwork'].front_default;
+    let typesHtml = pokemon.types.map(typeInfo => `<span>${typeInfo.type.name}</span>`).join(''); 
+    pokemonDetails.innerHTML = addPokemonDetailsHtml(i, pokemons = allPokemons,pokemonDetails,pokemon,imageUrl,typesHtml);
+}
+
+function addPokemonDetailsHtml(i, pokemons = allPokemons,pokemonDetails,pokemon,imageUrl,typesHtml){
+    return   `<div class="overlay-container" >
+        <h2>${pokemon.name}</h2>
+        <img onclick="closePokemonDetails()" src="${imageUrl}">
+        <div class="types-container">${typesHtml}</div>
+        <img onclick="swipeLeft()" class="left-right-logo" src="./img/left.svg" alt="left">
+        <img onclick="swipeRight()" class="left-right-logo" src="./img/right.svg" alt="right">
+    </div>`;
+}
+
+function swipeLeft() {
+    if (currentPokemonIndex > 0) {
+        currentPokemonIndex--;
+        openPokemonDetails(currentPokemonIndex);
+    }
+}
+
+function swipeRight() {
+    if (currentPokemonIndex < allPokemons.length - 1) {
+        currentPokemonIndex++;
+        openPokemonDetails(currentPokemonIndex);
+    }
+}
+
+function closePokemonDetails(){
+    document.getElementById('overlay').classList.add('d-none');
 }
