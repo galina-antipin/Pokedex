276cf1364fbb6ca3293a0596a8146cec363bc4ca
Galina Antipin <galina.antipin@gmail.com>
2024-07-29 14:34:39 +0200

    neu

--- script.js
+++ script.js
@@ -2,23 +2,24 @@ let allPokemons = [];
 let currentStart = 1;
 let currentEnd = 20;
 let currentPokemonIndex = 0;
+let filteredPokemons = [];
 
-function showLoadingPage() {
+function showLoadingScreen() {
     document.getElementById('loadingScreen').classList.remove('d-none');
 }
 
-function hideLoadingPage() {
+function hideLoadingScreen() {
     document.getElementById('loadingScreen').classList.add('d-none');
 }
 
 async function fetchData(start, end) {
-    showLoadingPage();
+    showLoadingScreen();
     for (let i = start; i <= end; i++) {
         let response = await fetch(`https://pokeapi.co/api/v2/pokemon/${i}`);
         let responseAsJson = await response.json();
         allPokemons.push(responseAsJson);
     }
-    hideLoadingPage();
+    hideLoadingScreen();
     renderPokemonData();
 }
 
@@ -39,7 +40,7 @@ function addPokemonHtml(pokemon, imageUrl, typesHtml, typeClass, i) {
     return `
         <div class="pokemon-card ${typeClass}" onclick="openPokemonDetails(${i})">
             <h2 class="pokemon-name">${capitalizeFirstLetter(pokemon.name)}</h2>
-            <img src="${imageUrl}">
+            <img src="${imageUrl}" alt="Pokemon">
             <div class="types-container">${typesHtml}</div>
         </div>
     `;
@@ -54,44 +55,63 @@ function loadMore() {
 function filterAndShowPokemons(filterWord) {
     let value = document.getElementById('searchBox').value;
     if (value.length < 3) {
+        renderPokemonData();
         return;
     }
-    let filteredPokemons = allPokemons.filter(pokemon => pokemon.name.toLowerCase().includes(filterWord.toLowerCase()));
-    renderPokemonData(filteredPokemons);
-    value.innerHTML = '';
+    filteredPokemons = allPokemons.filter(pokemon => pokemon.name.toLowerCase().includes(value.toLowerCase()));
+    renderPokemonData(filteredPokemons.slice(0, 10));   
 }
 
 fetchData(currentStart, currentEnd);
 
-function openPokemonDetails(i, pokemons = allPokemons) {
-    currentPokemonIndex = i;
-    document.getElementById('overlay').classList.remove('d-none');
-    document.getElementById('body').classList.add('stop-scroll');
-    let pokemonDetails = document.getElementById('overlay');
-    let pokemon = pokemons[i];
-    let imageUrl = pokemon.sprites['other']['official-artwork'].front_default;
-    let typesHtml = pokemon.types.map(typeInfo => `<span>${typeInfo.type.name}</span>`).join('');
-    pokemonDetails.innerHTML = addPokemonDetailsHtml(i, pokemons = allPokemons, pokemonDetails, pokemon, imageUrl, typesHtml,);
+function openPokemonDetails(i) {
+    if (filteredPokemons.length > 0 && i < filteredPokemons.length) {
+        currentPokemonIndex = i;
+        openPokemonDetailsOfFilteredPokemons();
+    } else {
+        currentPokemonIndex = i;
+        document.getElementById('overlay').classList.remove('d-none');
+        document.getElementById('body').classList.add('stop-scroll');
+        let pokemon = allPokemons[i];
+        renderPokemonDetails(pokemon);
+        const overlay = document.getElementById('overlay');
+        overlay.addEventListener('click', closePokemonDetails);
+    }
 }
 
-function addPokemonDetailsHtml(i, pokemons = allPokemons, pokemonDetails, pokemon, imageUrl, typesHtml) {
+function openPokemonDetailsOfFilteredPokemons() {
+    document.getElementById('overlay').classList.remove('d-none');
+    document.getElementById('body').classList.add('stop-scroll');
+    let pokemon = filteredPokemons[currentPokemonIndex];
+    renderPokemonDetails(pokemon);
+}
+
+function renderPokemonDetails(pokemon) {
+    let pokemonDetails = document.getElementById('overlay');
+    let imageUrl = pokemon.sprites['other']['official-artwork'].front_default;
+    let typesHtml = pokemon.types.map(typeInfo => `<span>${typeInfo.type.name}</span>`).join('');
+    pokemonDetails.innerHTML = addPokemonDetailsHtml(pokemon, imageUrl, typesHtml);
+}
+
+function addPokemonDetailsHtml(pokemon, imageUrl, typesHtml) {
     let typeClass = pokemon.types[0].type.name;
-    return `<div class="overlay-container">
-                  <div class="card-above ${typeClass}">
-                      <h3 onclick="closePokemonDetails()">${capitalizeFirstLetter(pokemon.name)}</h3> 
-                      <img onclick="closePokemonDetails()"src="${imageUrl}" alt="Pokemon">
-                  </div>
-                  <div class="pokemon-details"  onclick="closePokemonDetails()">  
-                      <div> HP:</span><span> ${pokemon.stats[0].base_stat}</div>
-                      <div> Attack: ${pokemon.stats[1].base_stat}</div>
-                      <div> Defense: ${pokemon.stats[2].base_stat}</div>
-                      <div> Speed: ${pokemon.stats[5].base_stat}</div>
-                  </div>
-                  <div class="left-right-logo-container">
-                      <img id="" onclick="swipeLeft()" class="left-right-logo" src="./img/left.svg" alt="left">
-                      <img onclick="swipeRight()" class="left-right-logo" src="./img/right.svg" alt="right">
-                  </div>
-            </div>`;
+    return `
+        <div class="overlay-container" onclick="event.stopPropagation();">
+            <div class="card-above ${typeClass}">
+                <h3>${capitalizeFirstLetter(pokemon.name)}</h3> 
+                <img src="${imageUrl}" alt="Pokemon" onclick="closePokemonDetails()">
+            </div>
+            <div class="pokemon-details">  
+                <div> HP: <span>${pokemon.stats[0].base_stat}</span></div>
+                <div> Attack: <span>${pokemon.stats[1].base_stat}</span></div>
+                <div> Defense: <span>${pokemon.stats[2].base_stat}</span></div>
+                <div> Speed: <span>${pokemon.stats[5].base_stat}</span></div>
+            </div>
+            <div class="left-right-logo-container">
+                <img onclick="swipeLeft(); event.stopPropagation();" class="left-right-logo" src="./img/left.svg" alt="left">
+                <img onclick="swipeRight(); event.stopPropagation();" class="left-right-logo" src="./img/right.svg" alt="right">
+            </div>
+        </div>`;
 }
 
 function capitalizeFirstLetter(string) {
@@ -106,7 +126,7 @@ function swipeLeft() {
 }
 
 function swipeRight() {
-    if (currentPokemonIndex < allPokemons.length - 1) {
+    if (currentPokemonIndex < allPokemons.length) {
         currentPokemonIndex++;
         openPokemonDetails(currentPokemonIndex);
     }
@@ -115,4 +135,6 @@ function swipeRight() {
 function closePokemonDetails() {
     document.getElementById('overlay').classList.add('d-none');
     document.getElementById('body').classList.remove('stop-scroll');
+    const overlay = document.getElementById('overlay');
+    overlay.removeEventListener('click', closePokemonDetails);
 }
