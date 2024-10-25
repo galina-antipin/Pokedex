8e4c13609c080232aec447a5133de7f88cb4839d
Galina Antipin <galina.antipin@gmail.com>
2024-10-25 18:16:14 +0200

    adited card details overlay

--- script.js
+++ script.js
@@ -107,6 +107,7 @@ function addPokemonDetailsHtml(pokemon, imageUrl, typesHtml) {
                 <h3>${capitalizeFirstLetter(pokemon.name)}</h3> 
                 <img src="${imageUrl}" alt="Pokemon" onclick="closePokemonDetails()">
             </div>
+            <div class="card-bottom">
             <div class="pokemon-details">  
                 <div> HP: <span>${pokemon.stats[0].base_stat}</span></div>
                 <div> Attack: <span>${pokemon.stats[1].base_stat}</span></div>
@@ -116,7 +117,7 @@ function addPokemonDetailsHtml(pokemon, imageUrl, typesHtml) {
             <div class="left-right-logo-container">
                 <img onclick="swipeLeft(); event.stopPropagation();" class="left-right-logo" id="left-button" src="./img/left.svg" alt="left">
                 <img onclick="swipeRight(); event.stopPropagation();" class="left-right-logo" src="./img/right.svg" alt="right">
-            </div>
+            </div> 
         </div>`;
 }
 
